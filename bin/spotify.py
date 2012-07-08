984e3d4d7b135a6be55f582366ef005589774cb9
Florian Ludwig <f.ludwig@greyrook.com>
2012-07-08 16:58:12 +0200

    simple refactoring
     * use ~/.cache for spotifty install
     * do not use os.fork

--- bin/spotify.py
+++ bin/spotify.py
@@ -8,48 +8,42 @@ import os
 import subprocess
 import time
 
-spotify_bin = "/usr/bin/spotify"
+SPOTIFY_SHARE = os.path.expanduser('~/.cache/spotify-install/')
+
 
 class SpotifyPlayer(dbus.service.Object):
     def __init__(self, conn, object_path="/org/mpris/MediaPlayer2"):
         dbus.service.Object.__init__(self, conn, object_path)
 
+
 class SpotifyGnome(object):
 
     def __init__(self):
-        pid = os.fork()
+        print "Setting up DBus..."
+        bus_loop = DBusGMainLoop(set_as_default=True)
+        self.session_bus = dbus.SessionBus(mainloop=bus_loop)
 
-        if pid == 0:
-            time.sleep(1)
-            print "Launching Spotify..."
-            self.spotify = None
-            args = [spotify_bin]
-            os.execv(spotify_bin, args)
+        print "Connecting to NameOwnerChanged..."
+        bus = self.session_bus.get_object("org.freedesktop.DBus",
+                                          "/org/freedesktop/DBus")
+        bus.connect_to_signal("NameOwnerChanged",
+                              self.name_owner_changed,
+                              arg0="org.mpris.MediaPlayer2.spotify")
 
-        else:
-            print "Setting up DBus..."
-            bus_loop = DBusGMainLoop(set_as_default=True)
-            self.session_bus = dbus.SessionBus(mainloop=bus_loop)
+        print "Connect to MediaKeys..."
+        bus = self.session_bus.get_object("org.gnome.SettingsDaemon",
+                                          "/org/gnome/SettingsDaemon/MediaKeys")
+        bus.connect_to_signal("MediaPlayerKeyPressed",
+                              self.key_pressed)
+        self.mediakeys = dbus.Interface(bus,
+                                        "org.gnome.SettingsDaemon.MediaKeys")
 
-            print "Connecting to NameOwnerChanged..."
-            bus = self.session_bus.get_object("org.freedesktop.DBus",
-                                              "/org/freedesktop/DBus")
-            bus.connect_to_signal("NameOwnerChanged",
-                                  self.name_owner_changed,
-                                  arg0="org.mpris.MediaPlayer2.spotify")
+        os.environ['LD_LIBRARY_PATH'] = SPOTIFY_SHARE
+        subprocess.Popen([SPOTIFY_SHARE + 'spotify'], env=os.environ)
+        self.loop = gobject.MainLoop()
+        self.loop.run()
 
-            print "Connect to MediaKeys..."
-            bus = self.session_bus.get_object("org.gnome.SettingsDaemon",
-                                              "/org/gnome/SettingsDaemon/MediaKeys")
-            bus.connect_to_signal("MediaPlayerKeyPressed",
-                                  self.key_pressed)
-            self.mediakeys = dbus.Interface(bus,
-                                            "org.gnome.SettingsDaemon.MediaKeys")
-
-            self.loop = gobject.MainLoop()
-            self.loop.run()
-
-            print "Done"
+        print "Done"
 
     def name_owner_changed(self, name, before, after):
         print "Received NameOwnerChanged:"
